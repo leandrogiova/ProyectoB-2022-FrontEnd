a70eb7fb9def4da17aedb76db8ece0976f720909
leandrogiova <leandrogiovacchini@gmail.com>
2022-07-04 21:24:39 -0300

    seguimos con la vista

---
+++ src/app/models/mesaProducto.ts
@@ -0,0 +1,30 @@
+import { Producto } from "./producto";
+
+export class mesaProductos{
+
+    id: number;
+    numero_mesa: number;
+    listaProductos: Producto[];
+    estado: boolean;
+    fecha: Date;
+    precioTotal: number;
+    precioTemporal: number;
+    formaDePago: string;
+    detalle: string;
+
+    constructor(id: number, numero_mesa:number, listaProductos: Producto[], precioTotal: number ,estado:boolean, fecha:Date, precioTemporal: number, formaDePago: string, detalle: string){
+        this.id = id;
+        this.numero_mesa = numero_mesa;
+        this.listaProductos = listaProductos;
+        this.estado = estado;
+        this.fecha = fecha;
+        this.precioTotal = precioTotal;
+        this.precioTemporal = precioTemporal;
+        this.formaDePago = formaDePago;
+        this.detalle = detalle;
+    }
+
+
+
+
+}
